29d2bae9d7040d8fd78b4131daeb2596d7ebde1c
Matthew <memcalarney@wpi.edu>
2025-09-17 22:33:09 -0400

    Add NavBar component

---
+++ application/src/app/component/NavBar.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+function NavBar() {
+    const links = [
+        {
+            label: 'TaskMaster', 
+            path: '/'
+        },
+        {
+            label: 'Completed Tasks',
+            path: '/completedtasks'
+        },
+    ]
+
+    const renderedLinks = links.map((link) => {
+        return <Link key={link.label} href={link.path}>
+            {link.label}
+        </Link>
+    })
+
+    return (
+        <div>
+            {renderedLinks}
+        </div>
+    )
+}
+
+export default NavBar;
