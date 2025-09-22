68f1479afc8ee8d6e9ddd38c067e2da72ba7c825
Matthew <memcalarney@wpi.edu>
2025-09-22 17:59:43 -0400

    Add fields and helper functions to entity classes

--- application/src/app/entity/model.ts
+++ application/src/app/entity/model.ts
@@ -1,13 +1,85 @@
-export class Engineer {
+export class Task {
+    taskID: number
+    name: String
+    estimatedTime: number
+    assigned: boolean
+    completedTime: number
+    isComplete: boolean
 
+    constructor(taskID: number, name: String, estimatedTime: number, assigned: boolean, completedTime: number, isComplete: boolean) {
+        this.taskID = taskID
+        this.name = name
+        this.estimatedTime = estimatedTime
+        this.assigned = assigned
+        this.completedTime = completedTime
+        this.isComplete = isComplete
+    }
 }
 
-export class Task {
+export class Engineer {
+    engineerID: number
+    name: String
+    assignedTasks: Task[]
+    totalEstimatedTaskTime: number
 
+    constructor(engineerID: number, name: String, assignedTasks: Task[], totalEstimatedTaskTime: number) {
+        this.engineerID = engineerID
+        this.name = name
+        this.assignedTasks = assignedTasks
+        this.totalEstimatedTaskTime = totalEstimatedTaskTime
+    }
 }
 
 export class Manager {
+    engineerList: Engineer[]
+    taskList: Task[]
+    completedTaskList: Task[]
+    totalEstimatedUnassignedTaskTime: number
+    totalCompletedTaskTime: number
 
+    constructor(engineerList: Engineer[], taskList: Task[], completedTaskList: Task[], totalEstimatedUnassignedTaskTime: number, totalCompletedTaskTime: number) {
+        this.engineerList = engineerList
+        this.taskList = taskList
+        this.completedTaskList = completedTaskList
+        this.totalEstimatedUnassignedTaskTime = totalEstimatedUnassignedTaskTime
+        this.totalCompletedTaskTime = totalCompletedTaskTime
+    }
+
+    addEngineer(name: String) : void {
+
+    }
+
+    removeEngineer(engineerID: number) : void {
+
+    }
+
+    addTask(name: String, estimatedTime: number) : void {
+
+    }
+
+    removeTask(taskID: number) : void {
+
+    }
+
+    assignTask(taskID: number, engineerID: number) : void {
+
+    }
+
+    completeTask(taskID: number, completedMinutes: number) : void {
+
+    }
+
+    computeTotalEstimatedUnassignedTaskTime(taskList : Task[]) : number {
+        return 0
+    }
+
+    computeTotalEstimatedTaskTimeByEngineer(taskList : Task[]) : number {
+        return 0
+    }
+
+    computeTotalCompletedTaskTime(completedTaskList : Task[]) : number {
+        return 0
+    }
 }
 
 export class Model {
