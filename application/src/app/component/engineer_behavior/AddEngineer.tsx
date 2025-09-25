3fb7f2c0fe920d7a44ed74a853fd42fa1edc3565
Matthew <memcalarney@wpi.edu>
2025-09-25 01:49:14 -0400

    Add handleChange and handleSubmit functions to AddEngineer component

--- application/src/app/component/engineer_behavior/AddEngineer.tsx
+++ application/src/app/component/engineer_behavior/AddEngineer.tsx
@@ -1,12 +1,25 @@
+import { FormEvent, useState } from "react"
+
 function AddEngineer() {
+    const [engineerName, setEngineerName] = useState("")
+
+    const handleChange = (e : FormEvent) => {
+      const target = e.target as HTMLInputElement
+      setEngineerName(target.value)
+    }
+
+    const handleSubmit = (e : FormEvent) => {
+      e.preventDefault()
+    }
+
     return (
         <div className = "block mt-2">
             <h2 className = "subtitle is-6 has-text-weight-bold is-family-sans-serif has-text-warning-dark">Add Engineer</h2>
             <h3 className = "subtitle is-7 is-family-sans-serif has-text-warning-dark">Specify an Engineer Name to add an engineer to the Engineer Table</h3>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className = "field">
                   <label className = "label is-family-code is-size-7">Engineer Name:</label>
-                  <input className = "input is-info is-small" type="text" id="engineerName"/>
+                  <input className = "input is-info is-small" type="text" id="engineerName" value={engineerName} onChange={handleChange}/>
                 </div>
                 <button className = "button is-family-monospace is-link is-size-7" id = "submitEngineer">Submit New Engineer</button>
               </form>
